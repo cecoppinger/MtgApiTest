7c3a81a54484f5c5fa40c23ba38b4681c6dcab2c
Clarence Coppinger <thea.trio@gmail.com>
2018-11-06 11:49:42 -0600

    Add project files.

---
+++ MtgApiTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MtgApiTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
